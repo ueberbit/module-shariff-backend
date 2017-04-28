4c89d1e229b4b2285d79e67dd401ccdaf49b5902
Steffen Rühlmann <steffen@undpaul.de>
2017-04-28 09:30:26 +0200

    DKRFTN-1707: fix typo in ShariffBackend class

--- src/ShariffBackend.php
+++ src/ShariffBackend.php
@@ -54,7 +54,7 @@ class ShariffBackend implements ShariffBackendInterface {
       return $cache[$cid];
     }
 
-    $backend_settins = \Drupal::config('shariff_backend.settings')->getRawData();
+    $backend_settings = \Drupal::config('shariff_backend.settings')->getRawData();
     // Simulate share counts if configured in settings file.
     if (empty($backend_settings['simulate_counts'])) {
       // Alter the URL if needed.
